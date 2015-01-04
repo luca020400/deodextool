ba92dbfb0502a07faabe2f1cbb5fd484695b11c1
luca020400 <luca.stefani.ge1@gmail.com>
2015-01-04 19:28:04 +0100

    Add API support

--- deodex.sh
+++ deodex.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Every *.odex in odex folder will be deodex and the clesses.dex will be moved into the classes_dex folder"
+echo -ne "Enter API level and press enter : "
+read API
 
 for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar -Duser.language=en oat2dex.jar $l.odex $l.dex; done
-for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar baksmali.jar -a 21 -x $l.dex -o $l; done
+for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar baksmali.jar -a $API -x $l.dex -o $l; done
 for l in `ls odex/*.odex | sed "s/.odex//"`; do a=`echo $l | sed "s/odex//"` && mkdir -p classes_dex/$a && java -jar smali.jar -a 21 $l -o classes_dex/$a/classes.dex; done
 
 echo "Done"
