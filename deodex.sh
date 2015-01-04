feb7e8f62f0d220d708e9f92fa3cc9e03ee21009
luca020400 <luca.stefani.ge1@gmail.com>
2015-01-04 18:51:17 +0100

    Fix derp

--- deodex.sh
+++ deodex.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Every *.odex in odex folder will be deodex and the clesses.dex will be renamed to $odexname.classes.dex
+echo "Every *.odex in odex folder will be deodex and the clesses.dex will be renamed to $odexname.classes.dex into odex folder"
 
 for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar -Duser.language=en oat2dex.jar $l.odex $l.dex; done
 for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar baksmali-2.0.3.jar -a 21 -x $l.dex -o $l; done
 for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar smali-2.0.3.jar -a 21 $l -o $l.classes.dex; done
+
+echo "Done"
