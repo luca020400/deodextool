40a6d390a82b69939b2284d4dc1c1d99c02334e9
luca020400 <luca.stefani.ge1@gmail.com>
2015-01-04 19:02:08 +0100

    V2

--- deodex.sh
+++ deodex.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Every *.odex in odex folder will be deodex and the clesses.dex will be renamed to $odexname.classes.dex into odex folder"
+echo "Every *.odex in odex folder will be deodex and the clesses.dex will be renamed to $odexname.classes.dex into the classes_dex folder"
 
 for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar -Duser.language=en oat2dex.jar $l.odex $l.dex; done
 for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar baksmali-2.0.3.jar -a 21 -x $l.dex -o $l; done
-for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar smali-2.0.3.jar -a 21 $l -o $l.classes.dex; done
+for l in `ls odex/*.odex | sed "s/.odex//"`; do a=`echo $l | sed "s/odex//"` && mkdir -p classes_dex/$a && java -jar smali-2.0.3.jar -a 21 $l -o classes_dex/$a/classes.dex; done
 
 echo "Done"
