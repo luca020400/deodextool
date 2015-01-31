d0b166df8b2f719e0396a4161ff92cf9b6da5070
luca020400 <luca.stefani.ge1@gmail.com>
2015-01-31 22:01:38 +0100

    Add jar framework support and fix up

--- deodex.sh
+++ deodex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dedeox () {
-a=`echo $1 | sed "s/odex//"`
+a=`echo $1 | sed "s/odexed//"`
 echo "Dedeoxing $1.apk"
 java -jar -Duser.language=en oat2dex.jar $1.odex $1.dex > /dev/null 2>&1
 java -jar baksmali.jar -a $API -x $l.dex -o $1
@@ -10,14 +10,24 @@ echo "$1.apk classes.dex created"
 echo
 }
 
-inject () {
-b=`echo $1 | sed "s/odex//"`
+inject_apk () {
+b=`echo $1 | sed "s/odexed//"`
 echo "Injecting $1 classex.dex into $1.apk"
 cp $1.apk deodexed/.
 zip -r deodexed/$b tmp/$b/classes.dex
 echo "classex.dex injected"
 echo
 }
+
+inject_jar () {
+b=`echo $1 | sed "s/odexed//"`
+echo "Injecting $1 classex.dex into $1.jar"
+cp $1.jar deodexed/.
+zip -r deodexed/$b tmp/$b/classes.dex
+echo "classex.dex injected"
+echo
+}
+
 
 cleanup () {
 rm -rf tmp
@@ -26,6 +36,7 @@ rm -rf tmp
 echo "Every *.apk and *.odex in odexed folder will be deodex and the clesses.dex will be injected into the *.apk"
 echo -ne "Enter API level and press enter : "
 read API
-for l in `ls odex/*.odex | sed "s/.odex//"`; do dedeox $l && inject $l; done > /dev/null 2>&1
+for l in `ls odexed/*.apk | sed "s/.apk//"`; do dedeox $l && inject_apk $l; done > /dev/null 2>&1
+for l in `ls odexed/*.jar | sed "s/.jar//"`; do dedeox $l && inject_jar $l; done > /dev/null 2>&1
 cleanup
 echo "Done"
