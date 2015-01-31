8caa1ed96f0493bf63ae0a203867fe27923f73b5
luca020400 <luca.stefani.ge1@gmail.com>
2015-01-31 21:19:42 +0100

    Add injecting support

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 Deodex Tool
 ===========
 
+You need the $app_name.apk and app_name.odex
+
 Commands
 
 ```bash
-mkdir odex
-cp *.odex odex/.
+mkdir odexed
+cp *.odex odexed/.
+cp *.apk odexed/.
 ./deodex.sh
 ```
--- deodex.sh
+++ deodex.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-echo "Every *.odex in odex folder will be deodex and the clesses.dex will be moved into the classes_dex folder"
+dedeox () {
+a=`echo $1 | sed "s/odex//"`
+echo "Dedeoxing $1.apk"
+java -jar -Duser.language=en oat2dex.jar $1.odex $1.dex > /dev/null 2>&1
+java -jar baksmali.jar -a $API -x $l.dex -o $1
+mkdir -p tmp/$a && java -jar smali.jar -a 21 $1 -o tmp/$a/classes.dex
+echo "$1.apk classes.dex created"
+echo
+}
+
+inject () {
+b=`echo $1 | sed "s/odex//"`
+echo "Injecting $1 classex.dex into $1.apk"
+cp $1.apk deodexed/.
+zip -r deodexed/$b tmp/$b/classes.dex
+echo "classex.dex injected"
+echo
+}
+
+echo "Every *.apk and *.odex in odexed folder will be deodex and the clesses.dex will be injected into the *.apk"
 echo -ne "Enter API level and press enter : "
 read API
-
-echo "Creating classes.dex"
-for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar -Duser.language=en oat2dex.jar $l.odex $l.dex; done > /dev/null 2>&1
-for l in `ls odex/*.odex | sed "s/.odex//"`; do java -jar baksmali.jar -a $API -x $l.dex -o $l; done
-for l in `ls odex/*.odex | sed "s/.odex//"`; do a=`echo $l | sed "s/odex//"` && mkdir -p classes_dex/$a && java -jar smali.jar -a 21 $l -o classes_dex/$a/classes.dex; done
-for l in `ls odex/*.odex | sed "s/.odex//"`; do rm -rf $l*; done
+echo
+for l in `ls odex/*.odex | sed "s/.odex//"`; do dedeox $l && inject $l; done > /dev/null 2>&1
+echo
 echo "Done"
