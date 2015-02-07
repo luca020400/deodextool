a44c369727112f3b766ffdff6aa0d55c317824ac
luca020400 <luca.stefani.ge1@gmail.com>
2015-02-07 15:40:32 +0100

    FixUp

--- deodex.sh
+++ deodex.sh
@@ -2,32 +2,31 @@
 
 dedeox () {
 a=`echo $1 | sed "s/odexed//"`
-echo "Dedeoxing $1.apk/jar"
-java -jar -Duser.language=en oat2dex.jar $1.odex $1.dex > /dev/null 2>&1
-java -jar baksmali.jar -a $API -x $l.dex -o $1
-mkdir -p tmp/$a && java -jar smali.jar -a 21 $1 -o tmp/$a/classes.dex
+echo "Dedeoxing $a.apk/jar"
+java -jar -Duser.language=en oat2dex.jar $1.odex tmp/$a.dex > /dev/null 2>&1
+java -jar baksmali.jar -a $API -x tmp/$a.dex -o tmp/$a
+mkdir -p tmp/$a && java -jar smali.jar -a 21 tmp/$a -o tmp/$a/classes.dex
 echo "$1.apk classes.dex created"
 echo
 }
 
 inject_apk () {
-b=`echo $1 | sed "s/odexed//"`
-echo "Injecting $1 classex.dex into $1.apk"
+a=`echo $1 | sed "s/odexed//"`
+echo "Injecting $a classex.dex into $a.apk"
 cp $1.apk deodexed/.
-zip -r deodexed/$1.apk tmp/$b/classes.dex
+zip -r deodexed/$a.apk tmp/$a/classes.dex
 echo "classex.dex injected"
 echo
 }
 
 inject_jar () {
-b=`echo $1 | sed "s/odexed//"`
-echo "Injecting $1 classex.dex into $1.jar"
+a=`echo $1 | sed "s/odexed//"`
+echo "Injecting $a classex.dex into $a.jar"
 cp $1.jar deodexed/.
-zip -r deodexed/$1.jar tmp/$b/classes.dex
+zip -r deodexed/$1.jar tmp/$a/classes.dex
 echo "classex.dex injected"
 echo
 }
-
 
 cleanup () {
 rm -rf tmp
@@ -38,5 +37,5 @@ echo -ne "Enter API level and press enter : "
 read API
 for l in `ls odexed/*.apk | sed "s/.apk//"`; do dedeox $l && inject_apk $l; done > /dev/null 2>&1
 for l in `ls odexed/*.jar | sed "s/.jar//"`; do dedeox $l && inject_jar $l; done > /dev/null 2>&1
-cleanup
+#cleanup
 echo "Done"
